537a3c7586adcb8707dafb03b8d4fb4365c8e8c0
Sarah Whalley <s.whalley1130@gmail.com>
2023-03-15 13:20:21 -0600

    Adds all neccessary functions for book

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title="And Then There Were None", page_count=0):
+        self.title=title
+        self.page_count=page_count
+
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, num):
+        if type(num) is int:
+            self._page_count=num
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+    page_count=property(get_page_count, set_page_count)
+
+book=Book("Titlehere")
+print(book.title)
+print(book.page_count)
+book.page_count=200
+print(book.page_count)
